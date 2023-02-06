af3d36592f5898aa6ab58e89a9177cbad0e805d7
Jiahua Zhang <hughzhanghz@gmail.com>
2023-02-05 22:52:37 -0500

    add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
+import styles from './styles/app.css';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
