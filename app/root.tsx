5ce63c28757f3d5088d748ddcaa8a7d79563fe90
Jiahua Zhang <hughzhanghz@gmail.com>
2023-02-08 17:28:15 -0500

    bugfix

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   Scripts,
   ScrollRestoration
 } from "@remix-run/react";
-import styles from './styles/app.css';
+import styles from '~/styles/app.css';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
