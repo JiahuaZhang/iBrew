879be2774296a0def24c8d74246cbee1e673b2ef
Jiahua Zhang <hughzhanghz@gmail.com>
2023-02-14 17:38:56 -0500

    organize auth routes

--- app/routes/(auth)/logout.tsx
+++ app/routes/(auth)/logout.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunction } from '@remix-run/node';
 import type { LoaderFunction } from 'react-router';
-import { authenticator } from '../services/auth.server';
+import { authenticator } from '../../services/auth.server';
 
 export const loader: LoaderFunction = async ({ request }) =>
   await authenticator.logout(request, { redirectTo: '/home' });
