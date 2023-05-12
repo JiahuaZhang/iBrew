10fbfbe7eb52b4dc469fc1105f3ef2ecba328f2e
Jiahua Zhang <hughzhanghz@gmail.com>
2023-05-12 18:17:49 -0400

    fix track layout

--- app/routes/home.track_.new/route.tsx
+++ app/routes/home.track_.new/route.tsx
@@ -1,7 +1,7 @@
 import { Tabs, type TabsProps } from 'antd';
 import { BiCog } from 'react-icons/bi';
 import { MdArticle } from 'react-icons/md';
-import { TrackContent } from '~/routes/home.track.new/TrackContent';
+import { TrackContent } from '~/routes/home.track_.new/TrackContent';
 
 // todo
 // global command to toggle between panel?
