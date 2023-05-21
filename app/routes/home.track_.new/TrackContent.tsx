9696872241ecd915e2e059e4533e38e1415b66d6
Jiahua Zhang <hughzhanghz@gmail.com>
2023-05-20 23:40:42 -0400

    update tag style

--- app/routes/home.track_.new/TrackContent.tsx
+++ app/routes/home.track_.new/TrackContent.tsx
@@ -1,7 +1,8 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { effect, signal } from '@preact/signals-react';
-import { Input, InputRef, Tag, Tooltip } from 'antd';
+import { Input, InputRef, Tag, Tooltip, notification } from 'antd';
 import { useEffect, useRef } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const title = signal('');
 const AutoResizingTextarea = () => {
@@ -25,7 +26,7 @@ const AutoResizingTextarea = () => {
   );
 };
 
-const tags = signal<string[]>(Array.from({ length: 30 }, (_, i) => `tag${i}`));
+const tags = signal<string[]>([]);
 const inputVisible = signal(false);
 const inputValue = signal('');
 const editInputIndex = signal(-1);
@@ -36,18 +37,28 @@ const editInputRef = signal<InputRef | null>(null);
 const handleClose = (removedTag: string) => tags.value = tags.value.filter((tag) => tag !== removedTag);
 
 const handleEditInputConfirm = () => {
-  const newTags = [...tags.value];
-  newTags[editInputIndex.value] = editInputValue.value;
-  tags.value = (newTags);
+  const existedTags = tags.value.filter((_, index) => index !== editInputIndex.value);
+  if (existedTags.includes(editInputValue.value)) {
+    notification.warning({ message: 'Tag already existed' });
+  } else {
+    const newTags = [...tags.value];
+    newTags[editInputIndex.value] = editInputValue.value;
+    tags.value = newTags;
+  }
+
   editInputIndex.value = -1;
   inputValue.value = '';
 };
 
 const handleInputConfirm = () => {
-  if (inputValue && tags.value.indexOf(inputValue.value) === -1) {
+  if (!inputValue.value) {
+    // notification.warning({ message: 'Tag cannot be empty' });
+  } else if (tags.value.includes(inputValue.value)) {
+    notification.warning({ message: 'Tag already existed' });
+  } else {
     tags.value = [...tags.value, inputValue.value];
   }
-  // todo, if already existed, notionfication add
+
   inputVisible.value = false;
   inputValue.value = '';
 };
@@ -59,15 +70,14 @@ effect(() => {
 });
 
 effect(() => editInputRef.value?.focus());
-
 const ContentTags = () => {
   return (
-    <div className='flex-wrap gap-y-2 inline-flex' >
+    <div className='pl-2 flex-wrap gap-y-2 inline-flex' >
       {tags.value.map((tag, index) => {
         if (editInputIndex.value === index) {
           return (
             <Input
-              className='w-[78px] align-top'
+              className='max-w-[100px] align-top text-xl py-1 px-2 mr-2'
               ref={r => editInputRef.value = r}
               key={tag}
               size="small"
@@ -81,10 +91,8 @@ const ContentTags = () => {
         const isLongTag = tag.length > 20;
         const tagElem = (
           <Tag
+            className='inline-flex items-center text-xl py-1 px-2 gap-2 border-2 border-slate-200 hover:border-blue-500'
             key={tag}
-            closable={true}
-            style={{ userSelect: 'none' }}
-            onClose={() => handleClose(tag)}
           >
             <span
               onDoubleClick={(e) => {
@@ -95,6 +103,9 @@ const ContentTags = () => {
             >
               {isLongTag ? `${tag.slice(0, 20)}...` : tag}
             </span>
+            <AiOutlineClose
+              onClick={() => handleClose(tag)}
+              className='cursor-pointer text-xs hover:text-red-400 text-gray-400' />
           </Tag>
         );
         return isLongTag ? (
@@ -108,7 +119,7 @@ const ContentTags = () => {
 
       {inputVisible.value ? (
         <Input
-          className='w-[78px] align-top'
+          className='max-w-[150px] align-top text-xl py-1 px-2'
           ref={r => inputRef.value = r}
           type="text"
           size="small"
@@ -118,7 +129,9 @@ const ContentTags = () => {
           onPressEnter={handleInputConfirm}
         />
       ) : (
-        <Tag onClick={() => inputVisible.value = true}>
+        <Tag
+          className='inline-flex items-center text-xl py-1 px-2 gap-2 cursor-pointer hover:border-2 hover:border-blue-500'
+          onClick={() => inputVisible.value = true}>
           <PlusOutlined /> New Tag
         </Tag>
       )}
