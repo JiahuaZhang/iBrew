9a55770a366aed6d9986215e525af01dd0badbc1
Jiahua Zhang <hughzhanghz@gmail.com>
2023-06-28 18:52:00 -0400

    fix link plugin

--- app/components/editor/plugin/MyLinkPlugin.tsx
+++ app/components/editor/plugin/MyLinkPlugin.tsx
@@ -1,8 +1,14 @@
 import { AutoLinkPlugin, createLinkMatcherWithRegExp, } from '@lexical/react/LexicalAutoLinkPlugin';
+import LexicalClickableLinkPlugin from '@lexical/react/LexicalClickableLinkPlugin';
+// todo? for paste & toggle
+// import {LinkPlugin} from '@lexical/react/LexicalLinkPlugin'
 
 const URL_REGEX = /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 const MATCHERS = [createLinkMatcherWithRegExp(URL_REGEX, (text) => text.startsWith('http') ? text : `https://${text}`)];
 
 export const MyLinkPlugin = () => {
-  return <AutoLinkPlugin matchers={MATCHERS} />;
+  return <>
+    <AutoLinkPlugin matchers={MATCHERS} />
+    <LexicalClickableLinkPlugin />
+  </>;
 };
